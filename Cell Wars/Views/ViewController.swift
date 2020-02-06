8f2f1ca56e80efe676f50dbc89559b5d4f2846a1
Trevor Walker <Walker123t@gmail.com>
2020-02-06 14:21:23 -0700

    simple fixes and some markdown

--- Cell Wars/Views/ViewController.swift
+++ Cell Wars/Views/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IBOutlets
     @IBOutlet weak var gameBoard: UIView!
     @IBOutlet weak var blueScore: UILabel!
     @IBOutlet weak var pinkScore: UILabel!
     @IBOutlet weak var playerTurnLabel: UILabel!
     
+    // MARK: - Properties
     let numViewPerRow = 8
     var cells = [String: TarBlobView]()
     var tarController = TarController(singlePlayer: false)
@@ -27,12 +29,18 @@ class ViewController: UIViewController {
         newGame()
     }
     
+    // Resets board and score
     func newGame() {
+        //Clears board
         cells.removeAll()
         gameBoard.subviews.forEach { $0.removeFromSuperview() }
+        //Calculates the width of our TarViews
         let width = (gameBoard.frame.width) / CGFloat(numViewPerRow)
+        //Resets TarController
         tarController.newGame()
+        //Updates Score and turn labels
         updateScoresAndTurn()
+        //Adds TarBlobViews to gameBoard
         for row in 0...numViewPerRow {
             for column in 0...numViewPerRow {
                 let rect = CGRect(x: CGFloat(column) * width, y: CGFloat(row) * width, width: width, height: width)
@@ -43,7 +51,7 @@ class ViewController: UIViewController {
                 cells[key] = tarBlob
             }
         }
-        
+        //Setting Colors for TarBlobViews
         for tar in tarController.board {
             switch tar.value.faction {
             case .blue:
@@ -55,24 +63,36 @@ class ViewController: UIViewController {
             }
         }
     }
-    
+    /**
+     Updates Turn label and score labels
+     */
     func updateScoresAndTurn() {
         playerTurnLabel.text = tarController.currentPlayer == .blue ? "Blue's Turn" : "Pink's Turn"
         blueScore.text = "Blue Score: \(tarController.blueCount)"
         pinkScore.text = "Pink Score: \(tarController.pinkCount)"
     }
-    
+    //calls newGameWhenPressed
     @IBAction func restartPressed(_ sender: Any) {
         newGame()
     }
+    /**
+     Called when a tar is being moved
+     
+     - Parameter tarBlobView - Holds the current View that we are moving to
+     */
     func didMoveTar(tarBlobView: TarBlobView) {
+        //Unwraps background color
         guard let backgroundColor = tarBlobView.backgroundColor else {return}
+        //Makes sure that our selected tar is a legal move
         if backgroundColor == .lightGray || backgroundColor == .darkGray {
+            //Updates all of our tars to the proper colors and faction status
             for tarBlob in tarController.moveTo(tarBlobView.index) {
                 cells["\(tarBlob.index)"]?.tar = tarBlob.tar
             }
+            //updates scores and player turn
             updateScoresAndTurn()
         } else {
+            //if not a legal move then we cancel the move
             tarController.cancelMove()
         }
         for tar in cells where tar.value.tar.faction == nil {
@@ -82,12 +102,15 @@ class ViewController: UIViewController {
     
     func didSelectTar(tarBlobView: TarBlobView) {
         let viableMoves = tarController.getViableMoves(index: tarBlobView.index)
-        
-        for tarBlob in viableMoves.duplicate {
-            cells["\(tarBlob)"]?.backgroundColor = .darkGray
-        }
-        for tarBlob in viableMoves.teleport {
-            cells["\(tarBlob)"]?.backgroundColor = .lightGray
+        if viableMoves.duplicate.count > 0 || viableMoves.teleport.count > 0 {
+            for tarBlob in viableMoves.duplicate {
+                cells["\(tarBlob)"]?.backgroundColor = .darkGray
+            }
+            for tarBlob in viableMoves.teleport {
+                cells["\(tarBlob)"]?.backgroundColor = .lightGray
+            }
+        } else {
+            tarController.cancelMove()
         }
     }
 }
@@ -127,12 +150,12 @@ extension ViewController: TarControllerDelegate {
             winner = "Tie Game!"
         }
         let alert = UIAlertController(title: winner, message: "Play Again?", preferredStyle: .alert)
-
+        
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { (_) in
             self.newGame()
         }))
         alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-
+        
         self.present(alert, animated: true)
     }
 }
